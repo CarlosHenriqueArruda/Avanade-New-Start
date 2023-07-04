1d5c2e17b5b214b42f53d660e69f6d4008d1c9e1
CarlosHenriqueArruda <carloshenriquearruda82@gmail.com>
2023-07-04 18:41:01 -0300

    Fundamentos Java Script

---
+++ JavaScript/Aula 1/script.js
@@ -0,0 +1,7 @@
+console.log("Boa noite pessoal!");
+console.info("Console de informação");
+console.warn("Cuidado");
+
+window.alert("Janela de Alerta");
+window.confirm("Tudo bem pessoal?");
+window.prompt("Qual o seu nome?");
